44f1b7a2963c2434e8d8bc6f91eefb1ee627ed91
盛合慶喜 <moriai0615@moriaiyoshinobunoMacBook-Pro.local>
2021-07-08 20:49:16 +0900

    Won/Lost message added

--- DiceGame.c
+++ DiceGame.c
@@ -4,12 +4,21 @@
 int main(void){
   int a = 0;
   int b = 0;
-  
+  char name[8];
+
+  printf("What is your name?\n");
+  scanf("%s",name);
+  printf("Hello, %s\n",name);
   printf("Rolling the dice...\n");
 
   a = rand() % 6 + 1;
   b = rand() % 6 + 1;
 
   printf("Die 1: %d \nDie 2: %d \nTotal value: %d\n", a, b, a+b);
+
+  if(a+b>7){
+    printf ("you won!\n");
+  } else{
+    printf ("you lose!\n");
   }
 }
